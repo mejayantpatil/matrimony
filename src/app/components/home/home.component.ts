db1098594d9be86a3f76a5081d24812b57b0f090
Jayant Patil <mejayantpatil@gmail.com>
2022-08-25 20:20:59 +0530

    Spinner postion fixed

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -14,7 +14,7 @@ export class HomeComponent implements OnInit {
    }
 
   ngOnInit(): void {
-    this.data = this.userSerivce.getUsers();
+    this.data = this.userSerivce.getUsers(); 
   }
 
   viewProfile(id: string) {
